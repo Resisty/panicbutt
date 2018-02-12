f2dd9c623b2fa87750aa3a64b1133a40e10b8b2a
Edward Olsen <eolsen@ad.davenport.edu>
2018-02-12 12:28:30 -0500

    Reddit Link Plugin

---
+++ opt/panicbutt/plugins/reddit.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# =======================================
+#
+#  File Name : reddit.py
+#
+#  Purpose : take r/subreddit and make it a clickable link
+#
+#  Creation Date : 12-02-2018
+#
+#  Last Modified : Mon 12 February 2018 10:31:27 AM EST
+#
+#  Created By : Edward Olsen
+#
+# ========================================
+
+import slackbot.bot
+import re
+import random
+import traceback
+
+REDDIT_REGEX = "((\/r\/)|(r\/))(\w+)"
+REDDIT = re.compile(REDDIT_REGEX, re.IGNORECASE)
+@slackbot.bot.listen_to(REDDIT)
+def reddit(message, *groups):
+
+	matches = re.search(REDDIT_REGEX, message_text)         
+	subreddit = matches.group(4) 
+    msg ="http://reddit.com/r/{}".format(subreddit)
+    message.reply(msg)
